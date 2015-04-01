355a36bcd7b3e8b7213e787fdb457c001046c9bb
Soumyadeep <soumyadeep9@gmail.com>
2015-04-01 06:53:31 +0530

    Broken Packet Function Added

--- main.cpp
+++ main.cpp
@@ -34,17 +34,64 @@ Robot_Config makeRobot(Leg_Config leg1,Leg_Config leg2,Leg_Config leg3,Leg_Confi
 
 void sendCommand(Robot_Config myRobot)
 {
-    command.preamble = 'n';
+    command.preamble = 0xaa;
+    command.info_byte = 0b00000000;
     command.myRobot = myRobot;
 
     sPort.Write(&command, sizeof(botPacket));
 }
 
+void sendBrokenPacket(Robot_Config myRobot)
+{
+    Robot_Config newRobot;
+    int angle[4];
+    Leg_Config leg[4];
+    
+    newRobot.leg[0].theta[0] =72;
+    newRobot.leg[1].theta[0] =75;
+    newRobot.leg[2].theta[0] =75;
+    newRobot.leg[3].theta[0] =80;
+	
+    for(int i=0;i<4;i++)
+    {	
+	for(int j=1;j<4;j++)
+	{
+		newRobot.leg[i].theta[j] =0;
+	}
+	leg[i] = makeLeg(newRobot.leg[i].theta[0],newRobot.leg[i].theta[1],newRobot.leg[i].theta[2],newRobot.leg[i].theta[3]);
+    }
+    newRobot = makeRobot(leg[0],leg[1],leg[2],leg[3]);
+    for(int i=0;i<10;i++)
+    {
+	for(int i=0;i<4;i++)
+	{
+	    for(int j=1;j<4;j++)
+	    {
+		newRobot.leg[i].theta[j] += (myRobot.leg[i].theta[j]/10);
+	    }
+	    leg[i] = makeLeg(newRobot.leg[i].theta[0],newRobot.leg[i].theta[1],newRobot.leg[i].theta[2],newRobot.leg[i].theta[3]);
+	}
+	Robot_Config sendRobot = makeRobot(leg[0],leg[1],leg[2],leg[3]);
+	sendCommand(sendRobot);
+	//usleep(1500000);
+    }
+    for(int i=0;i<4;i++)
+    {
+	for(int j=0;j<4;j++)
+	{
+	    newRobot.leg[i].theta[j] += (myRobot.leg[i].theta[j]%10);
+	}
+	leg[i] = makeLeg(newRobot.leg[i].theta[0],newRobot.leg[i].theta[1],newRobot.leg[i].theta[2],newRobot.leg[i].theta[3]);
+    }
+    Robot_Config sendRobot = makeRobot(leg[0],leg[1],leg[2],leg[3]);
+    sendCommand(sendRobot);
+
+}
 
 int main(int argc, char **argv)
 {
 
-/*	if(!sPort.Open("/dev/ttyACM0", 57600))
+	if(!sPort.Open("/dev/ttyUSB0", 57600))
 	{
 		perror("Could not Open Serial Port s0 :");
 		exit(0);
@@ -61,9 +108,10 @@ int main(int argc, char **argv)
 	}
 	Robot_Config myRobot = makeRobot(leg[0],leg[1],leg[2],leg[3]);
 
-	sendCommand(myRobot);
-*/
-	printf("%d",getAngle(153));
+//	sendCommand(myRobot);
+	sendBrokenPacket(myRobot);
+
+//	printf("%d",getAngle(153));
 	
 	return 0;
 }
