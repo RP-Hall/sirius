6420b074fee234426fc9a7b3a3f22506d044036a
Soumyadeep <soumyadeep9@gmail.com>
2015-04-02 04:35:14 +0530

    Matrix Added for Yaw Motor

--- main.cpp
+++ main.cpp
@@ -88,6 +88,8 @@ void sendBrokenPacket(Robot_Config myRobot)
 
 }
 
+int mat[4][5] = { { 95,75,55,35,15 }, {52,72,92,112,132}, {95,75,55,35,15}, {60,80,100,120,140}};
+
 int main(int argc, char **argv)
 {
 
@@ -98,22 +100,29 @@ int main(int argc, char **argv)
 	}
 	int angle[4];
 	Leg_Config leg[4];
-	for (int k = 0; k < 9; k++)
-	  {
+	int num_packets,t;
+	scanf("%d",&num_packets);
+	for (int k = 0; k < num_packets; k++)
+	{
 	    for(int i=0;i<4;i++)
-	      {
+	    {
 		for(int j=0;j<4;j++)
-		  {
-		    scanf("%d",&angle[j]);
-		  }
+		{
+		    if(j==0)
+		    {
+			scanf("%d",&t);
+			angle[j] = mat[i][t+2];
+		    }
+		    else scanf("%d",&angle[j]);
+		}
 		leg[i] = makeLeg(angle[0],angle[1],angle[2],angle[3]);
-	      }
+	    }
 	    Robot_Config myRobot = makeRobot(leg[0],leg[1],leg[2],leg[3]);
 	    
 	    sendCommand(myRobot);
-	    //sendBrokenPacket(myRobot);
-	    usleep(700000);
-	  }
+
+	    usleep(1000000);
+	 }
 //	printf("%d",getAngle(153));
 	
 	return 0;
