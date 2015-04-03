ea400db5f3a93d1b129e56c946389da73017e028
Keshav Sarraf <keshav.sarraf@iitkgp.ac.in>
2015-04-02 21:40:40 -0700

    accceptable_angles

--- matlab_inverseK/angle_conversion_odd.m
+++ matlab_inverseK/angle_conversion_odd.m
@@ -2,6 +2,6 @@ function theta_send = angle_conversion_odd(theta_in)
 theta_out = zeros(1,3);
 theta_out(2) = theta_in(2)+90;
 theta_out(3) = theta_in(3)+90;
-theta_out(1) = theta_in(1)+60;
+theta_out(1) = 60-theta_in(1);
 theta_send = [-1,theta_out(2),theta_out(3),theta_out(1)];
 end
