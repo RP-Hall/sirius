2e8ba50a605c1b80edbcd5f23dfb03d3d1713c93
Keshav Sarraf <keshav.sarraf@iitkgp.ac.in>
2015-04-02 20:40:17 -0700

    accceptable_angles

--- matlab_inverseK/inversekinematci.m
+++ matlab_inverseK/inversekinematci.m
@@ -9,10 +9,24 @@ x = 0;
 y = -120;
 phi = -60;
 
+fileID = fopen('acceptable_angles.txt','w');
+fprintf(fileID,'%c \n','a');
+% fclose(fileID);
+
+
 for phi = 0:-1:-180
     [theta,load] = inversekinematics(x,y,phi);
     if(load)
         continue;
     end
-    theta_out = round(angle_conversion(theta))
+    theta_out = [-1,round(angle_conversion(theta))];
+%     fileID = fopen('acceptable_angles.txt','a');
+    
+    fprintf(fileID,'%d ',theta_out);
+    fprintf(fileID,'\n');
+
+%     fclose(fileID);
 end
+
+
+
