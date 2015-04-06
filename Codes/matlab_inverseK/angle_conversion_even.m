ca7b537f150e690b4ea696af20d31a37a7c343f1
Keshav Sarraf <keshav.sarraf@iitkgp.ac.in>
2015-04-06 15:54:17 -0700

    shifted the matlab files to the codes folder

---
+++ Codes/matlab_inverseK/angle_conversion_even.m
@@ -0,0 +1,7 @@
+function theta_send = angle_conversion_even(theta_in)
+theta_out = zeros(1,3);
+theta_out(2) = theta_in(2)+90; 
+theta_out(3) = theta_in(3)+90;
+theta_out(1) = 240+theta_in(1);
+theta_send = [-1,theta_out(2),theta_out(3),theta_out(1)];
+end
